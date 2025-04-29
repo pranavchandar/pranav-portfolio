c6f2f3e804bc47dbd12857fbdb6cc7759ad6f8a3
pranavchandar <pranavchandar19@gmail.com>
2025-04-30 02:29:29 +0530

    home page overlay issues resolved

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,22 +5,25 @@ import { NavigationComponent } from '../navigation/navigation.component'; // Adj
 import { FooterComponent } from '../footer/footer.component'; // Adjust path
 
 @Component({
-selector: 'app-home',
-standalone: true,
-imports: [CommonModule, NavigationComponent, FooterComponent],
-templateUrl: './home.component.html',
-styleUrls: ['./home.component.scss']
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, NavigationComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-@ViewChild('homeContainer') homeContainer!: ElementRef;
+  @ViewChild('homeContainer') homeContainer!: ElementRef;
 
-constructor(private router: Router) {}
+  constructor(private router: Router) {}
 
-handleLogoClick() {
-const homeElement = this.homeContainer.nativeElement as HTMLElement;
-homeElement.classList.add('fade-out');
-setTimeout(() => {
-  this.router.navigate(['/secret']);
-}, 500);
-}
+  handleLogoClick() {
+    const homeElement = this.homeContainer.nativeElement as HTMLElement;
+    homeElement.classList.add('fade-out');
+
+    // Add a 1-second delay to match the black overlay's fade-out transition (2 seconds)
+    // We set the timeout duration to the CSS transition duration (2000ms)
+    setTimeout(() => {
+      this.router.navigate(['/secret']);
+    }, 2000); // <--- Increased delay to 2000ms (2 seconds)
+  }
 }
