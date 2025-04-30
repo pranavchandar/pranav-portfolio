9a1ea8f6a890747b43b1025b3e634dfb5d99a342
pranavchandar <pranavchandar19@gmail.com>
2025-05-01 02:38:45 +0530

    all good except the mobile view sub menu

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,23 +1,39 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
-selector: 'app-navigation',
-standalone: true,
-imports: [CommonModule, RouterLink, RouterLinkActive],
-templateUrl: './navigation.component.html',
-styleUrl: './navigation.component.scss'
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
-@Output() logoClick = new EventEmitter<void>();
-isMenuOpen = false;
+  @Output() logoClick = new EventEmitter<void>();
+  isMenuOpen = false;
+  isSubmenuOpen = false;
 
-toggleMenu() {
-this.isMenuOpen = !this.isMenuOpen;
-}
+  constructor(private elementRef: ElementRef) {}
 
-navigateToEasterEgg() {
-this.logoClick.emit();
-}
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleSubmenu(event: MouseEvent): void {
+    event.stopPropagation(); // Prevent dropdown toggle on click bubbling
+    this.isSubmenuOpen = !this.isSubmenuOpen;
+  }
+
+  navigateToEasterEgg(): void {
+    this.logoClick.emit();
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent): void {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.isSubmenuOpen = false;
+    }
+  }
 }
